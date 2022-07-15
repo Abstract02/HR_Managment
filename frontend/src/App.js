5984ae4a7ff0cfe1aabf260d4b0cacfba733c853
Abstract02 <adarsh526222@gmail.com>
2022-07-15 18:47:45 +0530

    routing done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,30 @@ import './App.css';
 import Login from './components/Login';
 import Profile from './components/Profile';
 import Admin from './components/Admin';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-     <Login />
+     <Router>
+        <Routes>
+          <Route exact path = '/' element = {
+            <Login />
+          } />
+          <Route path = '/admin' element = {<div>
+            <Admin />
+            </div>} />
+        <Route path = '/profile' element = {<div>
+            
+            <Profile />
+            </div>} />
+            </Routes>
+      </Router>
     </div>
   );
 }
