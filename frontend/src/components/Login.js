bcf1c7f6a059653029e6b59029f43184dbd4a8a2
Abstract02 <adarsh526222@gmail.com>
2022-07-15 16:53:55 +0530

    adding login page

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,33 @@
+function Login() {
+    
+    /* const handleChange = (e) => {
+
+        const { name, value } = e.target;
+        setuser({
+            ...user,
+            [name]: value
+        })
+    } */
+
+    return (
+        <>
+            <div className="bg-gray-800 h-screen flex justify-around ">
+                <div className="m-auto  w-3/12  rounded-lg ">
+                    <h1 className="p-5 text-5xl font-serif bg-zinc-700 flex justify-center">Login</h1>
+                    <div className="bg-zinc-400 flex flex-col">
+                        <input type="text" placeholder="Enter your Email" name="email"  className="m-4 rounded-md p-3"></input>
+                        <input type="text" placeholder="Enter your password" name="password"  className="m-4 rounded-md p-3"></input>
+                        <div className="flex flex-col">
+                            <button type="submit"  className="m-4 text-2xl text-white bg-slate-800 p-2 rounded-md hover:bg-zinc-700 hover:text-black">LOGIN</button>
+                            <h2 className="m-auto">-----------------------OR-----------------------</h2>
+                            <button type="submit"  className="m-4 text-2xl text-white bg-slate-800 p-2 rounded-lg  hover:bg-zinc-700 hover:text-black">REGISTER</button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Login;
